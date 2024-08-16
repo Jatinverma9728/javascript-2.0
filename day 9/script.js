8ec1ec391c7d9e2b7119ceb4accfb2b91e3a6706
Jatinverma9728 <somveerparjapati123456@gmail.com>
2024-08-16 23:03:57 +0530

    new file add tic tac toe game

---
+++ day 9/script.js
@@ -0,0 +1,53 @@
+let boxes = document.querySelectorAll(".box");
+let resetbtn = document.querySelector("#reset-btn");
+let newBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector("#msg")
+
+let turnO = true; //PlayerX , PlayerO
+
+const winPatterns = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [0, 1, 2],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO) {
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      box.innerText = "X";
+      turnO = true;
+    }
+    box.disabled = true;
+
+    checkWinner();
+  });
+});
+
+// const showWinner = (winner) => {
+//     msgContainer.innerText = `Congrulations, Winner is ${winner}`;
+//     msgContainer.classList.remove("hide"); 
+// }
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1val = boxes[pattern[0]].innerText;
+    let pos2val = boxes[pattern[1]].innerText;
+    let pos3val = boxes[pattern[2]].innerText;
+
+    if (pos1val != "" && pos2val != "" && pos3val != "") {
+      if (pos1val === pos2val && pos2val === pos3val) {
+        console.log("Winner");
+        showWinner(pos1val);
+      }
+    }
+  }
+};

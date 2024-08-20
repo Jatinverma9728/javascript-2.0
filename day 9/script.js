c09986a0eebd6b5c9ee67364706c2e6ae1562cfe
Jatinverma9728 <somveerparjapati123456@gmail.com>
2024-08-20 20:52:17 +0530

    update

--- day 9/script.js
+++ day 9/script.js
@@ -1,9 +1,11 @@
 let boxes = document.querySelectorAll(".box");
 let resetbtn = document.querySelector("#reset-btn");
 let newBtn = document.querySelector("#new-btn");
-let msgContainer = document.querySelector("#msg")
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#message");
 
-let turnO = true; //PlayerX , PlayerO
+let turnO = true; // PlayerX, PlayerO
+let moveCount = 0; // Move counter to track the number of moves
 
 const winPatterns = [
   [0, 1, 2],
@@ -11,7 +13,6 @@ const winPatterns = [
   [6, 7, 8],
   [0, 3, 6],
   [1, 4, 7],
-  [0, 1, 2],
   [2, 5, 8],
   [0, 4, 8],
   [2, 4, 6],
@@ -27,15 +28,17 @@ boxes.forEach((box) => {
       turnO = true;
     }
     box.disabled = true;
+    moveCount++; // Increment the move counter
 
     checkWinner();
   });
 });
 
-// const showWinner = (winner) => {
-//     msgContainer.innerText = `Congrulations, Winner is ${winner}`;
-//     msgContainer.classList.remove("hide"); 
-// }
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations, Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  disabledBoxes();
+};
 
 const checkWinner = () => {
   for (let pattern of winPatterns) {
@@ -45,9 +48,40 @@ const checkWinner = () => {
 
     if (pos1val != "" && pos2val != "" && pos3val != "") {
       if (pos1val === pos2val && pos2val === pos3val) {
-        console.log("Winner");
+        console.log("Winner", pos1val);
         showWinner(pos1val);
+        return; // Exit the function if we have a winner
       }
     }
   }
+
+  // Check for draw condition if no winner is found
+  if (moveCount === 9) {
+    msg.innerText = "It's a Draw!";
+    msgContainer.classList.remove("hide");
+    disabledBoxes();
+  }
 };
+
+const disabledBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+  moveCount = 0; // Reset the move counter when starting a new game
+};
+
+const resetgame = () => {
+  turnO = true;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+};
+
+newBtn.addEventListener("click", resetgame);
+resetbtn.addEventListener("click", resetgame);
